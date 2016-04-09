90c9ab53decce7eb2ec9dad50aca27eb32baa964
Daniel Jones <tortxof@gmail.com>
2016-04-09 17:09:00 -0400

    Use peewee orm.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 
 import os
+import datetime
 
-from flask import Flask, request, redirect
-
-import database
+from flask import Flask, request, redirect, g
+from peewee import *
 
 app = Flask(__name__)
 
@@ -12,11 +12,38 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
 
-db = database.Database('/data/data.db')
+db = SqliteDatabase('/data/data.db')
+
+class BaseModel(Model):
+    class Meta():
+        database = db
+
+class Signup(BaseModel):
+    name = CharField()
+    email = CharField()
+    time = DateTimeField()
+
+db.connect()
+db.create_tables([Signup], safe=True)
+db.close()
+
+@app.before_request
+def before_request():
+    g.db = db
+    g.db.connect()
+
+@app.after_request
+def after_request(request):
+    g.db.close()
+    return request
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    db.signup_create(request.form.to_dict())
+    Signup.create(
+        name = request.form.get('name', ''),
+        email = request.form.get('email', ''),
+        time = datetime.datetime.now()
+        )
     return redirect(request.form.get('next'))
 
 if __name__ == '__main__':
